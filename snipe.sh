5d09d85196242c22edc9bab70649a16643aafd8c
chris <christopherjosephdobbins@gmail.com>
2021-12-06 10:38:54 -0500

    change bid timing to 500ms before auction end

--- snipe.sh
+++ snipe.sh
@@ -2,14 +2,15 @@
 
 . .credentials
 ITEM_ID="$1"
-token_last_updated=$(echo "$(date +%s) - $(date -r token.txt +%s)" | bc -l)
+token_last_updated=$(echo "$(gdate +%s) - $(gdate -r token.txt +%s)" | bc -l)
 if [[ ! -n "$2" ]]; then
     echo "Must provide a max bid value"
     exit 1
 fi
 max_bid=$2
 echo $max_bid
-timer=$(date "+%s")
+timer=$(gdate "+%s")
+milli_timer=$(gdate "+%s%3N")
 # time constants
 # some have the unit preceding the number to
 # prevent the base from changing
@@ -17,6 +18,7 @@ let H_48=172800
 let H_24=86400
 let H_16=57600
 let H_12=43200
+let H_8=28800
 let H_4=14400
 let H_2=7200
 let H_1=3600
@@ -54,9 +56,9 @@ response=$(echo $(curl -s 'https://buyerapi.shopgoodwill.com/api/ItemDetail/GetI
 end_time=$(gdate --date=$(echo $response | jq .endTime | tr -d '"')"PST" "+%s")
 server_time=$(gdate --date=$(echo $response | jq .serverTime | tr -d '"')"PST" "+%s")
 let time_left=end_time-server_time
-now=$(date "+%s")
+now=$(gdate "+%s")
 let offset=now-server_time
-let time_with_offset=$(date "+%s")+offset
+let time_with_offset=$(gdate "+%s")+offset
 let local_time_left=end_time-time_with_offset
 winning_bidder=$(echo $(echo $response | jq .bidHistory.bidSummary[0].buyerId | tr -d '"'))
 top_bid=$(echo $(echo $response | jq .bidHistory.bidSummary[0].amount | tr -d '"'))
@@ -65,14 +67,18 @@ bid_increment=$(echo $(echo $response | jq .bidIncrement | tr -d '"'))
 bid_to_beat=$(echo "scale=2; ${top_bid}+${bid_increment}" | bc -l)
 is_bid_enough=$(echo "scale=2; ${max_bid}>=${bid_to_beat}" | bc -l)
 title=$(echo $(echo $response | jq .description | sed -E 's/<[a-z]{1,3}>Title: ([0-9a-z]+)<\/[a-z]{1,3}>/\1/g;s/(Item Attributes|Functionality|Condition).*//g'))
-
+# echo '<p><b>Title:</b> Grundig Yacht Boy 400 Shortwave Radio LW/MW/SW/FM<br><br><b></p>' | sed -E 's/<[a-z]{1,}>/\
+# /g;s/<\/[a-z]{1,}>/\
+# /g'| tr -s '\n' | sed '/^$/d'
 echo "Title: "${title}
 echo "Top bid: "${top_bid}
 echo 'Time left in auction: '$(echo "scale=2;"${local_time_left}"/3600" | bc -l)'h'
 
 while [[ $is_bid_enough -eq 1 ]]; do
-    now=$(date "+%s")
+    let now=$(gdate "+%s")
+    let milli_now=$(gdate "+%s%3N")
     let elapsed_time=now-timer
+    # let elapsed_milli_time=milli_now-milli_timer
     if [[ $local_time_left -ge $H_24 ]]; then
         let polling_interval=${H_12}
     elif [[ $local_time_left -ge $H_12 ]]; then
@@ -99,23 +105,40 @@ while [[ $is_bid_enough -eq 1 ]]; do
         let polling_interval=60
     fi
     if [[ $elapsed_time -ge $polling_interval ]]; then
+        echo "Polling interval: " $polling_interval
         # get info about an auction
         response=$(echo $(curl -s 'https://buyerapi.shopgoodwill.com/api/ItemDetail/GetItemDetailModelByItemId/'${ITEM_ID}  -X "GET" -H "Content-Type: application/json" -H "Access-Control-Allow-Origin: *" -H "Accept: application/json" -H "Authorization: Bearer ${token}" -H "Accept-Language: en-us" -H "Accept-Encoding: gzip, deflate, br" -H "Host: buyerapi.shopgoodwill.com" -H "Access-Control-Allow-Credentials: true" -H "Origin: https://shopgoodwill.com" -H "Connection: keep-alive" -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36' -H "Referer: https://shopgoodwill.com/shopgoodwill/inprogress-auctions"))
         winning_bidder=$(echo $(echo $response | jq .bidHistory.bidSummary[0].buyerId | tr -d '"'))
         top_bid=$(echo $(echo $response | jq .bidHistory.bidSummary[0].amount | tr -d '"'))
         end_time=$(gdate --date=$(echo $response | jq .endTime | tr -d '"')"PST" "+%s")
+        milli_end_time=$(gdate --date=$(echo $response | jq .endTime | tr -d '"')"PST" "+%s%3N")
+
         seller_id=$(echo $(echo $response | jq .sellerId | tr -d '"'))
 
         # server time format: "2021-11-28T21:17:45.34"
         # must add TZ to end of string instead of setting TZ env var
         server_time=$(gdate --date=$(echo $response | jq .serverTime | tr -d '"')"PST" "+%s")
-        now=$(date "+%s")
+        milli_server_time=$(gdate --date=$(echo $response | jq .serverTime | tr -d '"')"PST" "+%s%3N")
+
+        now=$(gdate "+%s")
+        milli_now=$(gdate "+%s%3N")
+
         let time_left=end_time-server_time
+        let milli_time_left=milli_end_time-milli_server_time
+
         let offset=now-server_time
-        let time_with_offset=$(date "+%s")+offset
+        let milli_offset=milli_now-milli_server_time
+
+        let time_with_offset=$(gdate "+%s")+offset
+        let milli_time_with_offset=$(gdate "+%s%3N")-milli_offset
+
         let local_time_left=end_time-time_with_offset
-        timer=$(date "+%s")
-        let bid_to_beat=$(echo "scale=2; ${top_bid}+${bid_increment}" | bc -l)
+        let milli_local_time_left=milli_end_time-milli_time_with_offset
+
+        timer=$(gdate "+%s")
+        milli_timer=$(gdate "+%s%3N")
+
+        bid_to_beat=$(echo "scale=2; ${top_bid}+${bid_increment}" | bc -l)
 
         echo 'Time left in auction: '$(echo "scale=2;"${local_time_left}"/3600" | bc -l)'h'
         echo "End time:" $end_time
@@ -124,8 +147,10 @@ while [[ $is_bid_enough -eq 1 ]]; do
         sleep .9 # This is to prevent flooding the screen
     fi
 
-    let time_with_offset=$(date "+%s")+offset
+    let time_with_offset=$(gdate "+%s")+offset
+    let milli_time_with_offset=$(gdate "+%s%3N")+milli_offset
     let local_time_left=end_time-time_with_offset
+    let milli_local_time_left=milli_end_time-milli_time_with_offset
     
     should_poll_more_1=$(echo "${time_left} <= 5" | bc -l)
     should_poll_more_2=$(echo "${local_time_left} <= 5" | bc -l)
@@ -151,16 +176,26 @@ while [[ $is_bid_enough -eq 1 ]]; do
         server_time=$(gdate --date=$(echo $response | jq .serverTime | tr -d '"')"PST" "+%s")
         end_time=$(gdate --date=$(echo $response | jq .endTime | tr -d '"')"PST" "+%s")
         let time_left=end_time-server_time
-        let time_with_offset=$(date "+%s")+offset
+        let milli_time_left=milli_end_time-milli_server_time
+
+        let time_with_offset=$(gdate "+%s")+offset
+        let milli_time_with_offset=$(gdate "+%s%3N")-milli_offset
+
         let local_time_left=end_time-time_with_offset
-        let bid_to_beat=$(echo "scale=2; ${top_bid}+${bid_increment}" | bc -l)
+        let milli_local_time_left=milli_end_time-milli_time_with_offset
+
+        bid_to_beat=$(echo "scale=2; ${top_bid}+${bid_increment}" | bc -l)
         echo "time left: "$local_time_left
 
         # first condition is a reality check to see if 
         # the response is valid before placing a bid
-        if [[ (${time_left} -gt 0 && ${local_time_left} -gt 0) && (${time_left} -le 3 ||  ${local_time_left} -le 3) ]]; then
-            should_bid=$(echo "scale=2; ${max_bid}>=${top_bid}+${bid_increment}" | bc -l)
+        # if [[ (${time_left} -gt 0 && ${local_time_left} -gt 0) && (${time_left} -le 3 ||  ${local_time_left} -le 3) ]]; then
+        if [[ (${milli_time_left} -gt 0 && ${milli_local_time_left} -gt 0) && (${milli_time_left} -le 500 || ${milli_local_time_left} -le 500) ]]; then
+            let should_bid=$(echo "scale=2; ${max_bid}>=${top_bid}+${bid_increment}" | bc -l)
             echo $should_bid
+            if [[ $should_bid -eq 0 ]]; then 
+                echo "Not bidding; top bid is: "${top_bid}
+            fi
             # it is _un_necessary to delete the newline;
             # in fact, doing so would lead to an invalid
             # response
@@ -169,11 +204,14 @@ while [[ $is_bid_enough -eq 1 ]]; do
                 echo "Party time!"
                 curl -s "https://buyerapi.shopgoodwill.com/api/ItemBid/PlaceBid" -X "POST" -H 'Content-Type: application/json' -H 'Access-Control-Allow-Origin: *' -H 'Accept: application/json' -H 'Authorization: Bearer '${token} -H 'Accept-Language: en-us' -H 'Accept-Encoding: gzip, deflate, br' -H 'Host: buyerapi.shopgoodwill.com' -H 'Access-Control-Allow-Credentials: true' -H 'Origin: https://shopgoodwill.com' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15' -H 'Referer: https://shopgoodwill.com/' -H 'Connection: keep-alive' -H 'Content-Length:'${content_length} --data-binary '{"itemId":'$ITEM_ID',"quantity":1,"sellerId":'${seller_id}',"bidAmount":"'${max_bid}'"}'
             fi
-        elif [[ ${time_left} -le 0 || ${local_time_left} -le 0 ]]; then
+        elif [[ ${milli_time_left} -le 0 || ${milli_local_time_left} -le 0 ]]; then
             echo "auction is over!"
             exit 0
         fi
-        sleep .45
+        # Note: this is non-portable; the man page says that doing this 
+        # "will nearly guarantee that a shell script will not properly
+        # execute on another system"
+        sleep .15
     done
 done
 
